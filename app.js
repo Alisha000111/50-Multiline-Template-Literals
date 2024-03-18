21efd6c50d45f2b6fce4a663ec3bc9f1a00ea695
Alisha M Irfan <160208713+Alisha000111@users.noreply.github.com>
2024-03-18 16:59:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+// Using template literals to define a multiline string
+var myIdealDay = "My ideal day would involve:\n1. Waking up early and going for a jog.\n2. Spending a few hours typescript coding on a personal project.\n3. Ending the day by reading a horor book.";
+// Logging the multiline string to the console
+console.log(myIdealDay);
